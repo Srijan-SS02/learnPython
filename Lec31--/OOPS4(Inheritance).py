452fd946f7caf03258a7bf003d3dc9e2e58da66e
Srijan-SS02 <codeofficialsrijansriv@gmail.com>
2022-11-11 03:03:38 +0530

    Completed Inheritence

--- Lec31--/OOPS4(Inheritance).py
+++ Lec31--/OOPS4(Inheritance).py
@@ -1,9 +1,11 @@
 class employee():
-    def __init__(self,name) -> None:
+    def __init__(self,name, salary) -> None:
         self.name=name
+        self.salary=salary
 
 class bartender(employee):
-    pass
+    def __init__(self, name ) -> None:
+        super().__init__(name, salary='3000')
 
 
 class chef(employee):
@@ -11,7 +13,11 @@ class chef(employee):
 
 
 bartender1=bartender(name="Harry")
+bartender2=bartender(name="Ron")
 print(bartender1.name)
+print(bartender2.name , "\n")
+print(bartender1.salary)
 
-chef1=chef(name="Hermionee")
+chef1=chef(name="Hermionee", salary=25000)
 print(chef1.name)
+
